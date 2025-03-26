a863f341a9dc284f11bed2cce0c1ece74a9053cf
addifex <maxwell.schaefer@addifex.com>
2025-03-26 11:00:24 -0700

    Updated scripts to use the new root namespace

--- Runtime/Constants.cs
+++ Runtime/Constants.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 
-public static class Constants
+namespace Addifex.Kinematics
 {
-    public const float GROUND_CHECK = .01f;
-    public const float COLLISION_OFFSET = .001f;
-        
-    public static readonly Vector3[] Directions = 
+    public static class Constants
     {
-        Vector3.forward, Vector3.back, Vector3.left, Vector3.right,
-        Vector3.up, Vector3.down,
-        new Vector3(1, 0, 1).normalized, new Vector3(-1, 0, -1).normalized,
-        new Vector3(-1, 0, 1).normalized, new Vector3(1, 0, -1).normalized
-    };
+        public const float GROUND_CHECK = .01f;
+        public const float COLLISION_OFFSET = .001f;
+        
+        public static readonly Vector3[] Directions = 
+        {
+            Vector3.forward, Vector3.back, Vector3.left, Vector3.right,
+            Vector3.up, Vector3.down,
+            new Vector3(1, 0, 1).normalized, new Vector3(-1, 0, -1).normalized,
+            new Vector3(-1, 0, 1).normalized, new Vector3(1, 0, -1).normalized
+        };
+    }
 }
--- Runtime/Functions.cs
+++ Runtime/Functions.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 
-public static class Functions
+namespace Addifex.Kinematics
 {
-    public static (Vector3 bottom, Vector3 top) CreateCapsuleCastPoints(Vector3 position, float radius, float height)
+    public static class Functions
     {
-        Vector3 bottom = position + new Vector3(0, radius);
-        Vector3 top = position + new Vector3(0, height - radius);
+        public static (Vector3 bottom, Vector3 top) CreateCapsuleCastPoints(Vector3 position, float radius, float height)
+        {
+            Vector3 bottom = position + new Vector3(0, radius);
+            Vector3 top = position + new Vector3(0, height - radius);
         
-        return (bottom, top);
-    }
+            return (bottom, top);
+        }
 
-    public static float GetStepHeight(Vector3 position, RaycastHit hit)
-    {
-        return hit.point.y - position.y;
+        public static float GetStepHeight(Vector3 position, RaycastHit hit)
+        {
+            return hit.point.y - position.y;
+        }
     }
 }
